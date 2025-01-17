cbb99a4877eae1922135d0f2981387376f8a0a1a
Surbhi Sharma <159677261+Surbhi-Sharma-30@users.noreply.github.com>
2025-01-17 12:01:55 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,83 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
+                    <img src="image_uploads/11.jpg" alt="" />
+                    <p>Campus Portal is a portal where stduents get job hand to hand 
+
+one of the best plateform where students get different types of fresh jobs.
+                        <label>Companies hire those students who are fresher or exprience.</label></p>
+                    <div class="clearfix"></div>
+                    <p class="nam">Just Upload your cv and fill up all details correctly we will provide best way to go in best companies.<br/><br/><br/><br/></p>
+
+                </div>
+                <!--div class="top-inner second">
+                    <div class="col-md-6 top-text">
+                        <a href="single.html"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="col-md-6 top-text two">
+                        <a href="single.html"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Campus Portal <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                             </div>
+                        <div class="col-md-9 item-details">
+                                <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                           
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                                                     <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                                                   </div>
+                        <div class="col-md-9 item-details">
+                                                   <div class="td-post-date two"> </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
