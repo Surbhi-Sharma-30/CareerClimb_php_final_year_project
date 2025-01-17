25acc8412bdfcf53f6fce0fd16fd21969c9d1f64
Surbhi Sharma <159677261+Surbhi-Sharma-30@users.noreply.github.com>
2025-01-17 12:17:08 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>Campus Placement</h4>
+            <ul class="bottom">
+                <li>Designed By: surbhi</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="Amityadav@gmail.com">surbhisharma@gmail.com</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>7024496958</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>Near lal school gopalganj sagar.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: (+91) 7024496958 </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>Campus Portal</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
